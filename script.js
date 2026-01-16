907b064ea7f94bc4940224ef18f6fd1f298108b0
rascodee <irbadhganzz@gmail.com>
2026-01-16 17:40:47 +0700

    apdet kecil kecilan

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
